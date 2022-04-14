6bf85768424ec8cc4f829cfba666f138d7c4c871
Hansanghyeon <999@hyeon.pro>
2022-04-14 18:16:39 +0900

    fix: 스토리북 링크

--- .storybook/imwebTheme.js
+++ .storybook/imwebTheme.js
@@ -1,8 +1,6 @@
 import { create } from '@storybook/theming';
 
 export default create({
-  base: 'light',
-  brandTitle: 'My custom storybook',
-  brandUrl: 'https://example.com',
+  brandTitle: 'imweb storybook',
   brandImage: '/imweb-logo.svg',
 });
