0e29a8b2c8e126de30a571063de93902ba24baef
Hansanghyeon <999@hyeon.pro>
2022-04-15 14:08:32 +0900

    feat: 스토리북 클릭 깃헙으로 이동

--- .storybook/imwebTheme.js
+++ .storybook/imwebTheme.js
@@ -3,4 +3,5 @@ import { create } from '@storybook/theming';
 export default create({
   brandTitle: 'imweb storybook',
   brandImage: '/imweb-logo.svg',
+  brandUrl: 'https://github.com/Hansanghyeon/imstorybook',
 });
